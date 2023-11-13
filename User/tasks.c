f93334a5a9089e3f52ba62b79f17cc0236c4cba0
Poscape <893945798@qq.com>
2023-11-13 17:31:01 +0800

    fix led; improve sched

--- User/tasks.c
+++ User/tasks.c
@@ -6,15 +6,30 @@ int16_t magx = 0, magy = 0, magz = 0;
 
 extern int dutyCycleArray[6];
 
+/* Delay */
+INT16U gy86_delay = 10;
+INT16U kalman_delay = 10;
+INT16U send_delay = 10;
+INT16U oled_delay = 10;
+
+INT16U inner_loop_delay = 20;
+INT16U motor_delay = 20;
+
+INT16U outer_loop_delay = 40;
+INT16U blink_delay = 40;
+/* Delay End */
+
 /* Time */
 INT32U gy86_time = 0;
 INT32U kalman_time = 0;
 INT32U send_time = 0;
+INT32U oled_time = 0;
 
 INT32U inner_loop_time = 0;
 INT32U motor_time = 0;
 
 INT32U outer_loop_time = 0;
+INT32U blink_time = 0;
 /* Time End */
 
 /* Task Stacks */
@@ -64,7 +79,7 @@ void GY86Task(void *p_arg)
 		INT32U tick2 = OSTimeGet();
 		gy86_time = tick2 - tick1;
 
-		OSTimeDlyHMSM(0, 0, 0, 1);
+		OSTimeDlyHMSM(0, 0, 0, gy86_delay);
 	}
 }
 
@@ -79,7 +94,7 @@ void KalmanTask(void *p_arg)
 		INT32U tick2 = OSTimeGet();
 		kalman_time = tick2 - tick1;
 
-		OSTimeDlyHMSM(0, 0, 0, 1);
+		OSTimeDlyHMSM(0, 0, 0, kalman_delay);
 	}
 }
 
@@ -116,24 +131,23 @@ void SendTask(void *p_arg)
 		INT32U tick2 = OSTimeGet();
 		send_time = tick2 - tick1;
 
-		OSTimeDlyHMSM(0, 0, 0, 1);
+		OSTimeDlyHMSM(0, 0, 0, send_delay);
 	}
 }
 
 void OLEDTask(void *p_arg)
 {
-	int mode = 1;
+	INT32U tick1 = OSTimeGet();
+
+	int mode = 2;
 
 	while (1)
 	{
-		OLED_ShowString(1, 1, "T:");
-		OLED_ShowNum(1, 3, OSTime, 5);
-
-		OLED_ShowString(1, 9, "COS:");
-		OLED_ShowNum(1, 13, gy86_time, 4);
-
-		if (mode == 0)
+		if (mode == 0) // GY86 Mode
 		{
+			OLED_ShowString(1, 1, "T:");
+			OLED_ShowNum(1, 3, OSTime, 5);
+
 			OLED_ShowString(2, 1, "X");
 			OLED_ShowSignedNum(2, 2, accx, 4);
 			OLED_ShowSignedNum(2, 7, gyrox, 4);
@@ -149,8 +163,11 @@ void OLEDTask(void *p_arg)
 			OLED_ShowSignedNum(4, 7, gyroz, 4);
 			OLED_ShowSignedNum(4, 12, magz, 4);
 		}
-		else if (mode == 1)
+		else if (mode == 1) // Motor Mode
 		{
+			OLED_ShowString(1, 1, "T:");
+			OLED_ShowNum(1, 3, OSTime, 5);
+
 			OLED_ShowNum(2, 1, dutyCycleArray[0], 3);
 			OLED_ShowNum(2, 5, dutyCycleArray[1], 3);
 			OLED_ShowNum(3, 1, dutyCycleArray[2], 3);
@@ -158,8 +175,32 @@ void OLEDTask(void *p_arg)
 			OLED_ShowNum(4, 1, dutyCycleArray[4], 3);
 			OLED_ShowNum(4, 5, dutyCycleArray[5], 3);
 		}
+		else if (mode == 2) // Time Mode
+		{
+			OLED_ShowString(1, 1, "G:");
+			OLED_ShowNum(1, 3, gy86_time, 4);
+			OLED_ShowString(1, 9, "K:");
+			OLED_ShowNum(1, 11, kalman_time, 4);
+			OLED_ShowString(2, 1, "S:");
+			OLED_ShowNum(2, 3, send_time, 4);
+			OLED_ShowString(2, 9, "O:");
+			OLED_ShowNum(2, 11, oled_time, 4);
 
-		OSTimeDlyHMSM(0, 0, 0, 1);
+			OLED_ShowString(3, 1, "I:");
+			OLED_ShowNum(3, 3, inner_loop_time, 4);
+			OLED_ShowString(3, 9, "M:");
+			OLED_ShowNum(3, 11, motor_time, 4);
+
+			OLED_ShowString(4, 1, "O:");
+			OLED_ShowNum(4, 3, outer_loop_time, 4);
+			OLED_ShowString(4, 9, "B:");
+			OLED_ShowNum(4, 11, blink_time, 4);
+		}
+
+		INT32U tick2 = OSTimeGet();
+		oled_time = tick2 - tick1;
+
+		OSTimeDlyHMSM(0, 0, 0, oled_delay);
 	}
 }
 
@@ -184,7 +225,7 @@ void InnerLoopTask(void *p_arg)
 		INT32U tick2 = OSTimeGet();
 		inner_loop_time = tick2 - tick1;
 
-		OSTimeDlyHMSM(0, 0, 0, 2);
+		OSTimeDlyHMSM(0, 0, 0, inner_loop_delay);
 	}
 }
 
@@ -199,7 +240,7 @@ void MotorTask(void *p_arg)
 		INT32U tick2 = OSTimeGet();
 		motor_time = tick2 - tick1;
 
-		OSTimeDlyHMSM(0, 0, 0, 2);
+		OSTimeDlyHMSM(0, 0, 0, motor_delay);
 	}
 }
 
@@ -224,12 +265,13 @@ void OuterLoopTask(void *p_arg)
 		INT32U tick2 = OSTimeGet();
 		outer_loop_time = tick2 - tick1;
 
-		OSTimeDlyHMSM(0, 0, 0, 4);
+		OSTimeDlyHMSM(0, 0, 0, outer_loop_delay);
 	}
 }
 
 void BlinkTask(void *p_arg)
 {
+	INT32U tick1 = OSTimeGet();
 	BOOLEAN flag = 0;
 
 	while (1)
@@ -241,6 +283,9 @@ void BlinkTask(void *p_arg)
 
 		flag = !flag;
 
-		OSTimeDlyHMSM(0, 0, 0, 4);
+		INT32U tick2 = OSTimeGet();
+		blink_time = tick2 - tick1;
+
+		OSTimeDlyHMSM(0, 0, 0, blink_delay);
 	}
 }
